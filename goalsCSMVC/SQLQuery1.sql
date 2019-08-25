8c910867c4c77ddb638ea1d59fdfc51b00ded1e4
chantebryana <chantebryana@gmail.com>
2019-08-25 11:24:05 -0500

    Playing w DB; create fake data

---
+++ goalsCSMVC/SQLQuery1.sql
@@ -0,0 +1,9 @@
+select * from Moon1
+
+insert into Moon1 (Date) values ('2019-08-25') 
+
+UPDATE Moon1 SET Down = '20:38' WHERE Id = 4
+
+ALTER table Moon1 ADD Down TIME
+
+ALTER TABLE Moon1 DROP COLUMN FullMoonDate
---
+++ goalsCSMVC/goalsCSMVC/Models/SunRiseSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace goalsCSMVC.Models
+{
+    public class SunRiseSet
+    {
+        public int Id { get; set; }
+        
+        //I'm still figuring this out! Def a work in progress.
+        [DataType(DataType.Date)]
+        public date BobLobLaw { get; set; }
+        public 
+    }
+}
